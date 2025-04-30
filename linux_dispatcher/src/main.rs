677ccc35f5aa6cc7506c53e64f4c7243116fed41
Vrtgs <vrtgs@vrtgs.xyz>
2025-04-30 08:43:22 +0300

    update nix and fix linux dispatcher build

--- linux_dispatcher/src/main.rs
+++ linux_dispatcher/src/main.rs
@@ -1,10 +1,16 @@
-#![cfg(target_os = "linux")]
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        use std::os::unix::net::UnixStream;
 
-use std::os::unix::net::UnixStream;
+        const SOCKET_PATH: &str =
+            include_str!("../../cloudflare-ddns/src/network_listener/linux/socket-path");
 
-const SOCKET_PATH: &str =
-    include_str!("../../cloudflare-ddns/src/network_listener/linux/socket-path");
-
-fn main() {
-    let _ = UnixStream::connect(SOCKET_PATH);
+        fn main() {
+            let _ = UnixStream::connect(SOCKET_PATH);
+        }
+    } else {
+        fn main() {
+            panic!("available on linux only")
+        }
+    }
 }
